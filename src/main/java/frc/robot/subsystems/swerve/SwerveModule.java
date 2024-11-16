27cc5dfe9c69031d6f1e89e7baf723a8ae1a0cff
CrolineCrois <maoninii@gmail.com>
2024-11-15 17:29:12 -0800

    swerve module (old setpoint generation)

--- src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
@@ -3,28 +3,41 @@ package frc.robot.subsystems.swerve;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkAnalogSensor;
+import com.revrobotics.SparkMaxAlternateEncoder;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
 
 public class SwerveModule {
 
     private final KrakenDriveMotor driveMotor;
     private final CANSparkMax steerMotor;
 
+    private final SparkAbsoluteEncoder steerEncoder;
+    private final SparkPIDController steerPIDController;
+
     private double offsetRads;
 
-    private static final double DRIVE_ROTATIONS_PER_METER = 0;
+    private static final double DRIVE_ROTATIONS_PER_METER = 0; // temporary value
 
     private static final double DRIVE_P = 1; // temporary value
     private static final double DRIVE_I = 0;
     private static final double DRIVE_D = 0;
     private static final double DRIVE_FF = 0;
+
+    private static final double STEER_P = 1; // temporary value
+    private static final double STEER_I = 0;
+    private static final double STEER_D = 0;
+    private static final double STEER_FF = 0;
 
 
     /** Constructs a Swerve Module.
@@ -37,6 +50,21 @@ public class SwerveModule {
     public SwerveModule(int drivePort, int steerPort, double offsetRads) {
 
         steerMotor = new CANSparkMax(steerPort, MotorType.kBrushless);
+        steerMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus5, 20); //increase position update frequency
+        steerMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus6, 20); //increase velocity update frequency
+
+        steerEncoder = steerMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
+        steerEncoder.setAverageDepth(8); //tune value as needed
+
+        steerPIDController = steerMotor.getPIDController();
+        steerPIDController.setFeedbackDevice(steerEncoder); //configure Steer to use the absolute encoder for closed loop PID feedback
+            steerPIDController.setP(STEER_P);
+            steerPIDController.setI(STEER_I);
+            steerPIDController.setD(STEER_D);
+            steerPIDController.setFF(STEER_FF);
+        steerPIDController.setPositionPIDWrappingEnabled(true); //enable PID wrapping
+            steerPIDController.setPositionPIDWrappingMinInput(0);
+            steerPIDController.setPositionPIDWrappingMaxInput(2 * Math.PI);
 
         driveMotor = new KrakenDriveMotor(drivePort);
         driveMotor.configPID(DRIVE_P, DRIVE_I, DRIVE_D, DRIVE_FF);
@@ -48,7 +76,37 @@ public class SwerveModule {
 
     }
 
-        /** Gets the current state of the swerve module.
+    /** Sets the desired state of this swerve module through setting the PID targets.
+     *
+     * @param state The desired SwerveModuleState
+     */
+    public void setDesiredState(SwerveModuleState state) {
+        Rotation2d currentAngle = getWrappedAngle();
+        SwerveModuleState optimized = SwerveModuleState.optimize(state, currentAngle); // SIYONA ITS THIS LINE FOR THE MATH IN FACT UPDATE THIS WHOLE FUNCTION
+        //UPDATE ^^
+
+        double targetAngleRads = optimized.angle.getRadians() - offsetRads;
+        double angleErrorRads = optimized.angle.minus(currentAngle).getRadians();
+
+        // Multiply by cos so we don't move quickly when the swerves are angled wrong
+        double targetVelocity = optimized.speedMetersPerSecond * Math.cos(angleErrorRads);
+
+        driveMotor.setVelocity(targetVelocity);
+
+        steerPIDController.setReference(targetAngleRads, ControlType.kPosition);
+    }
+
+    /** Sets the raw powers of the swerve module.
+     *
+     * @param drivePower The power for the drive motor
+     * @param steerPower The power for the steer motor
+     */
+    public void setRawPowers(double drivePower, double steerPower) {
+        driveMotor.setPower(drivePower);
+        steerMotor.set(steerPower);
+    }
+
+    /** Gets the current state of the swerve module.
      *
      * @return The current SwerveModulePosition of this module
      */
@@ -59,12 +117,12 @@ public class SwerveModule {
         );
     }
 
-        /** Gets the current angle of the module.
+    /** Gets the current angle of the module.
      *
      * @return Wrapped angle in radians from -pi to pi
      */
     public Rotation2d getWrappedAngle() {
-        double angleRads = 0;//get steer angle here
+        double angleRads = steerEncoder.getPosition();
         double wrappedAngleRads = MathUtil.angleModulus(angleRads + offsetRads);
 
         return new Rotation2d(wrappedAngleRads);
@@ -95,7 +153,7 @@ public class SwerveModule {
         return driveMotor.getAmpDraw();
     }
 
-        /** Gets the distance the distance driven by the drive motor.
+    /** Gets the distance the distance driven by the drive motor.
      *
      * @return The distance driven in meters.
      */
