93b78faec93553d2b4c9b0bc82b87f34f30a318c
eerinn <erinmicro9@gmail.com>
2025-01-15 20:13:34 -0800

    pid

--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -329,8 +329,8 @@ public class SwerveSubsystem extends SubsystemBase {
          //1.25/3.25 PID
          //
             new PPHolonomicDriveController(
-                new PIDConstants(1.25, 0, 0.0),
-                new PIDConstants(3.25, 0.0, 0.0)
+                new PIDConstants(1.25, 0.0, 0.0),
+                new PIDConstants(2, 0.0, 0.0)
             ),
 
             config,
