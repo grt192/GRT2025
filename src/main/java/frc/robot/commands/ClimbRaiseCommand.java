c8411b7fe98f8f9e6916afc38bb0297e4358b648
siyoyoCode <siyona101215@gmail.com>
2025-01-16 19:08:02 -0800

    fixed sparkmax follow errors

--- src/main/java/frc/robot/commands/ClimbRaiseCommand.java
+++ src/main/java/frc/robot/commands/ClimbRaiseCommand.java
@@ -6,14 +6,13 @@ import frc.robot.subsystems.ClimbSubsystem;
 /** Lowers climb fully when off the chain. Should run at the start of auton to ensure that both arms are at their lowest
  *  positions so that the robot may fit underneath the stage. */
 public class ClimbRaiseCommand extends Command {
-    private static final double LOWERING_SPEED = -0.9;
     private final ClimbSubsystem climbSubsystem;
     private final int speed = 0; //placeholder
+    private final double endingMotorPosition = 0.0;
     
     /** Constructs a new {@link ClimbLowerCommand}. */
     public ClimbRaiseCommand(ClimbSubsystem climbSubsystem) {
         this.climbSubsystem = climbSubsystem;
-
         this.addRequirements(climbSubsystem);
     }
 
@@ -24,7 +23,7 @@ public class ClimbRaiseCommand extends Command {
 
     @Override
     public boolean isFinished() {
-        return climbSubsystem.isLowered();
+        return climbSubsystem.getMotorPosition() == endingMotorPosition;
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -3,27 +3,22 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.EncoderConfig;
-import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.MAXMotionConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.config.MAXMotionConfig;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import frc.robot.Constants.ClimbConstants;
 
 
@@ -32,48 +27,44 @@ public class ClimbSubsystem extends SubsystemBase {
   private final SparkMax topMotor;
   private final SparkMax botMotor;
 
+  private final int botMotorID;
+
   private RelativeEncoder encoder;
   private final ClosedLoopConfig closedLoopConfig;
   private SparkMaxConfig sparkMaxConfig;
   private final SparkClosedLoopController steerPIDController;
   private final EncoderConfig encoderConfig;
+  private MAXMotionConfig maxMotionConfig;
 
   /** Creates a new ExampleSubsystem. */
   public ClimbSubsystem() {
     
     topMotor = new SparkMax(ClimbConstants.TOP_MOTOR_ID, MotorType.kBrushless);
     botMotor = new SparkMax(ClimbConstants.BOT_MOTOR_ID, MotorType.kBrushless);
-    botMotor.follow(topMotor);
+
+    botMotorID = 0; //have to change later
+
     steerPIDController = topMotor.getClosedLoopController();
 
     encoderConfig = new EncoderConfig();
-    // encoderConfig.inverted(true);
+    encoderConfig.inverted(true);
 
-    closedLoopConfig = new ClosedLoopConfig();//code copied from swerve, untested do NOT trust it
+    closedLoopConfig = new ClosedLoopConfig(); //code copied from swerve, untested do NOT trust it
     closedLoopConfig.feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
                     .positionWrappingEnabled(true)
                     .positionWrappingMinInput(0)
                     .positionWrappingMaxInput(1);
     
-    
     sparkMaxConfig = new SparkMaxConfig();
     sparkMaxConfig.apply(encoderConfig)
                   .apply(closedLoopConfig)
-                  .inverted(true);
-
+                  .inverted(true)
+                  .follow(botMotorID);
+                  
     topMotor.configure(sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    botMotor.configure(sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);    
     configurePID(ClimbConstants.CLIMB_P, ClimbConstants.CLIMB_I, ClimbConstants.CLIMB_D);
-
   }
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-
-  //sets the speed of the rollers
-
   
   public void configurePID(double p, double i, double d){
     closedLoopConfig.pid(p, i, d);
