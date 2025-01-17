3aebddbe7ca5c74cfaa06cf096d0e5ee1ea8926f
eerinn <erinmicro9@gmail.com>
2025-01-16 22:05:56 -0800

    testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -157,7 +157,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return new PathPlannerAuto("curve");
+    return new PathPlannerAuto("New Auto");
   }
 
   /**
