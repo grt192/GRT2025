9e75c3b697adfc33780e879123e964763e1c6fda
Tony Wu <tonyxywu@gmail.com>
2025-01-20 01:22:23 -0800

    half finished interfacing between vision & swerve

--- src/main/java/frc/robot/subsystems/Vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.Vision;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -11,7 +12,9 @@ import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.networktables.NetworkTable;
@@ -19,7 +22,9 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.Constants.VisionConstants;
+import frc.robot.subsystems.Vision.TimestampedVisionUpdate;
+import frc.robot.util.PolynomialRegression;
 public class VisionSubsystem extends SubsystemBase {
     private final PhotonCamera camera;
     private final PhotonPoseEstimator photonPoseEstimator;
@@ -34,8 +39,17 @@ public class VisionSubsystem extends SubsystemBase {
     private NetworkTableInstance ntInstance;
     private NetworkTable visionStatsTable;
     private StructPublisher visionPosePublisher;
-
     private Consumer<List<TimestampedVisionUpdate>> visionConsumer = (x) -> {};
+    
+    private List<TimestampedVisionUpdate> cameraVisionUpdate;
+
+    private PolynomialRegression xStdDevModel =
+        new PolynomialRegression(VisionConstants.STD_DEV_DIST,VisionConstants.X_STD_DEV,2);
+    private PolynomialRegression yStdDevModel =
+        new PolynomialRegression(VisionConstants.STD_DEV_DIST,VisionConstants.Y_STD_DEV,2);
+    private PolynomialRegression oStdDevModel = //standard deviation of the theta
+        new PolynomialRegression(VisionConstants.STD_DEV_DIST,VisionConstants.O_STD_DEV,1);
+
     public VisionSubsystem() {
         // Initialize the camera with its network table name
         camera = new PhotonCamera("1");
@@ -65,10 +79,19 @@ public class VisionSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        // Get the latest pipeline result
+        // Get the latest pipeline result the results are queued up since the last call
         List<PhotonPipelineResult> results = camera.getAllUnreadResults();
-
+        
         for (PhotonPipelineResult result : results){
+            cameraVisionUpdate = new ArrayList<>();
+            double timestamp = result.getTimestampSeconds();//get time stamp of results
+
+            double totalDistance = 0.0;
+            for (Pose3d tagPose : tagPose3ds) {
+                totalDistance += tagPose.getTranslation().getDistance(cameraPose.getTranslation());
+            }
+            double avgDistance = totalDistance / tagPose3ds.size();
+            double xStdDev = 
             if (result.hasTargets()) {
                 // Get estimated robot pose
                 Optional<EstimatedRobotPose> estimatedPose = photonPoseEstimator.update(result);
@@ -76,11 +99,23 @@ public class VisionSubsystem extends SubsystemBase {
                 if (estimatedPose.isPresent()) {
                     // Update the current pose
                     currentPose = estimatedPose.get().estimatedPose.toPose2d();
-                    // You might want to send this to other subsystems or log it
-                    visionPosePublisher.set(currentPose);
+                    // You might wanst to send this to other subsystems or log it
                 }
+
             }
+            cameraVisionUpdate.add(
+            new TimestampedVisionUpdate(
+                currentPose, //current x, y, and theta 
+                timestamp,
+                VecBuilder.fill(//standard deviation matrix
+                    singleTagAdjustment * singleTagStdDevScalar * xyStdDev * stdDevScalarAuto,
+                    singleTagAdjustment * singleTagStdDevScalar * xyStdDev * stdDevScalarAuto,
+                    singleTagAdjustment * singleTagStdDevScalar * thetaStdDev * stdDevScalarAuto))
+        )
+        visionConsumer.accept(cameraVisionUpdate);
+
         }
+        
 
     }
 
@@ -105,6 +140,6 @@ public class VisionSubsystem extends SubsystemBase {
      * @param consumer consumer to receive vision updates
      */
     public void setInterface(Consumer<List<TimestampedVisionUpdate>> consumer){
-        visionConsumer = consumer;
+        visionConsumer = consumer;//thiing for vision to interface with the swerve subsystem
     }
 }
