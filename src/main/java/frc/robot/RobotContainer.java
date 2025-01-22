588ba4547e1b3da188d6a129c9fdff401429a7f7
Jesse <jesse.chu@outlook.com>
2025-01-21 17:03:28 -0800

    Only start log once and don't use phoenix signal logs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -36,13 +36,13 @@ public class RobotContainer {
   private final FieldManagementSubsystem fieldManagementSubsystem =
     new FieldManagementSubsystem();
 
-  private final PhoenixLoggingSubsystem phoenixLoggingSubsystem =
-    new PhoenixLoggingSubsystem(fieldManagementSubsystem);
+  // private final PhoenixLoggingSubsystem phoenixLoggingSubsystem =
+    // new PhoenixLoggingSubsystem(fieldManagementSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     constructDriveController(); 
-    // startLog();
+    startLog();
     configureBindings();
   }
 
@@ -123,6 +123,6 @@ public class RobotContainer {
    */
   private void startLog(){
     DataLogManager.start();
-    // DriverStation.startDataLog(DataLogManager.getLog());
+    DriverStation.startDataLog(DataLogManager.getLog());
   }
 }
