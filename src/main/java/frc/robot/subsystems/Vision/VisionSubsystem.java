8b7e0f254693433de4a9faac1671a84a934b7a64
Tony Wu <tonyxywu@gmail.com>
2025-01-21 17:40:04 -0800

    sync

--- src/main/java/frc/robot/subsystems/Vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.Vision;
 
+import java.lang.annotation.Target;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +11,7 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.VecBuilder;
@@ -17,9 +19,12 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.VisionConstants;
@@ -39,6 +44,8 @@ public class VisionSubsystem extends SubsystemBase {
     private NetworkTableInstance ntInstance;
     private NetworkTable visionStatsTable;
     private StructPublisher visionPosePublisher;
+    private DoublePublisher visionDistPublisher;
+
     private Consumer<List<TimestampedVisionUpdate>> visionConsumer = (x) -> {};
     
     private List<TimestampedVisionUpdate> cameraVisionUpdate;
@@ -50,6 +57,7 @@ public class VisionSubsystem extends SubsystemBase {
     private PolynomialRegression oStdDevModel = //standard deviation of the theta
         new PolynomialRegression(VisionConstants.STD_DEV_DIST,VisionConstants.O_STD_DEV,1);
 
+    
     public VisionSubsystem() {
         // Initialize the camera with its network table name
         camera = new PhotonCamera("1");
@@ -70,11 +78,12 @@ public class VisionSubsystem extends SubsystemBase {
             CAMERA_TO_ROBOT
         );
 
-        ntInstance = NetworkTableInstance.getDefault();
-        visionStatsTable = ntInstance.getTable("VisionStats");
-        visionPosePublisher = visionStatsTable.getStructTopic(
-            "VisionPose", Pose2d.struct
-        ).publish();
+        // ntInstance = NetworkTableInstance.getDefault();
+        // visionStatsTable = ntInstance.getTable("VisionStats");
+        // visionPosePublisher = visionStatsTable.getStructTopic(
+        //     "VisionPose", Pose2d.struct
+        // ).publish();
+        initNT();
     }
 
     @Override
@@ -82,40 +91,58 @@ public class VisionSubsystem extends SubsystemBase {
         // Get the latest pipeline result the results are queued up since the last call
         List<PhotonPipelineResult> results = camera.getAllUnreadResults();
         
+        //loops through all snapshots of the camera stream
         for (PhotonPipelineResult result : results){
             cameraVisionUpdate = new ArrayList<>();
             double timestamp = result.getTimestampSeconds();//get time stamp of results
 
-            double totalDistance = 0.0;
-            for (Pose3d tagPose : tagPose3ds) {
-                totalDistance += tagPose.getTranslation().getDistance(cameraPose.getTranslation());
-            }
-            double avgDistance = totalDistance / tagPose3ds.size();
-            double xStdDev = 
-            if (result.hasTargets()) {
+
+
+            // Variables to track the closest target
+            
+            //checks if the camera detected any apriltags
+            if (result.hasTargets()){
+
+            double minDistance = Double.MAX_VALUE; // Start with a very large value
+            PhotonTrackedTarget closestTarget = null;
+    
+            //loops through all detected targets from the camera
+            for (PhotonTrackedTarget target : result.getTargets()) {
                 // Get estimated robot pose
                 Optional<EstimatedRobotPose> estimatedPose = photonPoseEstimator.update(result);
-                
                 if (estimatedPose.isPresent()) {
                     // Update the current pose
+                    //Transform3d tagToRobotTransform = result.getTargets();
+                    //Translation3d translation = result.getTra
                     currentPose = estimatedPose.get().estimatedPose.toPose2d();
                     // You might wanst to send this to other subsystems or log it
                 }
-
+                Transform3d cameraToTagTransform = target.getBestCameraToTarget();
+                Translation3d translation = cameraToTagTransform.getTranslation();
+                double distance = Math.sqrt(
+                    Math.pow(translation.getX(),2) +
+                    Math.pow(translation.getY(),2) +
+                    Math.pow(translation.getZ(),2) 
+                );
+                if (distance < minDistance){
+                    minDistance = distance;
+                    closestTarget = target;
+                }
             }
             cameraVisionUpdate.add(
             new TimestampedVisionUpdate(
-                currentPose, //current x, y, and theta 
-                timestamp,
+                timestamp, //current x, y, and theta 
+                currentPose,
                 VecBuilder.fill(//standard deviation matrix
-                    singleTagAdjustment * singleTagStdDevScalar * xyStdDev * stdDevScalarAuto,
-                    singleTagAdjustment * singleTagStdDevScalar * xyStdDev * stdDevScalarAuto,
-                    singleTagAdjustment * singleTagStdDevScalar * thetaStdDev * stdDevScalarAuto))
-        )
-        visionConsumer.accept(cameraVisionUpdate);
-
+                    xStdDevModel.predict(minDistance),
+                    yStdDevModel.predict(minDistance),
+                    oStdDevModel.predict(minDistance)))
+            );
+            visionConsumer.accept(cameraVisionUpdate);
+            visionDistPublisher.set(minDistance);
+            visionPosePublisher.set(currentPose);
         }
-        
+    }
 
     }
 
@@ -141,5 +168,14 @@ public class VisionSubsystem extends SubsystemBase {
      */
     public void setInterface(Consumer<List<TimestampedVisionUpdate>> consumer){
         visionConsumer = consumer;//thiing for vision to interface with the swerve subsystem
+    }
+
+    private void initNT(){
+        ntInstance = NetworkTableInstance.getDefault();
+        visionStatsTable = ntInstance.getTable("Vision Debug");
+        visionPosePublisher = visionStatsTable.getStructTopic(
+            "estimated pose", Pose2d.struct
+        ).publish();
+        visionDistPublisher = visionStatsTable.getDoubleTopic("dist").publish();
     }
 }
