7f3796f13e5d5fca10a48ffe929e250279ea3019
Jesse <jesse.chu@outlook.com>
2025-01-23 10:12:37 -0800

    set interface between vision and swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,6 +108,9 @@ public class RobotContainer {
       },
       swerveSubsystem
     );
+    visionSubsystem.setInterface(swerveSubsystem::addVisionMeasurements);
+  }
+
 
     xButton.onTrue(
       AutoAlignCommand.reefTest(swerveSubsystem));
