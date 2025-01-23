186c807b5ecb40039e4eb99ad2c0fdaeb33b9626
eerinn <erinmicro9@gmail.com>
2025-01-22 20:38:12 -0800

    source aligin

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -55,7 +55,8 @@ public class RobotContainer {
 
   private final SendableChooser<Command> autoChooser;
 
-  private Trigger rightBumper;
+  private Trigger xButton;
+  private Trigger squareButton
   boolean isCompetition = false;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -64,7 +65,8 @@ public class RobotContainer {
     // startLog();
     mechController = new CommandPS5Controller(1);
     
-    rightBumper = new Trigger(mechController.cross());
+    xButton = new Trigger(mechController.cross());
+    squareButton = new Trigger(mechController.square();)
 
     configureBindings();
     
@@ -114,10 +116,13 @@ public class RobotContainer {
       swerveSubsystem
     );
 
-    rightBumper.onTrue(
+    xButton.onTrue(
       AutoAlignCommand.reefTest(swerveSubsystem));
 
     visionSubsystem.setInterface(swerveSubsystem::addVisionMeasurements);
+    squareButton.onTrue(
+      AutoAlignCommand.sourceTest(swerveSubsystem));
+
   }
 
   
