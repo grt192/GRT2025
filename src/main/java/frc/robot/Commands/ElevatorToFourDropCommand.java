ed844e9a655d058592733647283afa8202fc2fd2
siyoyoCode <siyona101215@gmail.com>
2025-01-23 16:54:43 -0800

    elevator commands

---
+++ src/main/java/frc/robot/Commands/ElevatorToFourDropCommand.java
@@ -0,0 +1,42 @@
+package frc.robot.Commands;
+
+// import edu.wpi.first.networktables.NetworkTable;
+// import edu.wpi.first.networktables.NetworkTableEntry;
+// import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator.ElevatorState;
+import frc.robot.subsystems.Elevator.ElevatorSubsystem;
+
+/** Sends the elevator to its amp-scoring position. */
+public class ElevatorToFourDropCommand extends Command {
+    private ElevatorSubsystem elevatorSubsystem;
+    // private NetworkTableInstance nt;
+    // private NetworkTable table;
+    // private NetworkTableEntry entry;
+
+    /** Constructs a {@link ElevatorToAmpCommand} for the specified elevator. */
+    public ElevatorToFourDropCommand(ElevatorSubsystem elevatorSubsystem) {
+        this.addRequirements(elevatorSubsystem);
+        this.elevatorSubsystem = elevatorSubsystem;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+        // nt = NetworkTableInstance.getDefault();
+        // table = nt.getTable("elevator");
+        // entry = table.getEntry("position");
+        // entry.setInteger(3);
+
+    }
+
+    @Override
+    public void execute() {
+        this.elevatorSubsystem.setTargetState(ElevatorState.FOUR_DROP);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return elevatorSubsystem.atState(ElevatorState.FOUR_DROP);
+    }
+}
