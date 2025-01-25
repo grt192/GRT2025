e77d5373b70d5acd2615df9c62afb2dea2a80d04
Jesse <jesse.chu@outlook.com>
2025-01-24 20:35:30 -0800

    Formatted code

--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -20,8 +20,6 @@ import frc.robot.util.GRTUtil;
 
 import static frc.robot.Constants.SwerveConstants.*;
 
-import java.util.List;
-
 import static frc.robot.Constants.LoggingConstants.*;
 import static frc.robot.Constants.DebugConstants.*;
 
@@ -132,14 +130,12 @@ public class SwerveSubsystem extends SubsystemBase {
             MAX_VEL, MAX_VEL, MAX_OMEGA);
     }
 
-    public void addVisionMeasurements(List<TimestampedVisionUpdate> updates){
-        for (TimestampedVisionUpdate update : updates){
-            poseEstimator.addVisionMeasurement(
-                update.pose(), 
-                update.timestamp(),
-                update.stdDevs()
-            );
-        }
+    public void addVisionMeasurements(TimestampedVisionUpdate update){
+        poseEstimator.addVisionMeasurement(
+            update.pose(), 
+            update.timestamp(),
+            update.stdDevs()
+        );
     }
     /**
      * Gets the module positions.
