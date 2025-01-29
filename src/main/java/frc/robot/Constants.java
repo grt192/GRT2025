681abdd3cbad93363d5746d36a2e6dd94e70eca0
Jesse <jesse.chu@outlook.com>
2025-01-28 20:47:56 -0800

    Enable PIDF tuning through NT + Logging more things for spark max.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -82,15 +82,19 @@ public final class Constants {
     public static final double[] STEER_I = new double[] {0, 0, 0, 0};
     public static final double[] STEER_D = new double[] {0, 0, 0, 0};
     public static final double[] STEER_FF = new double[] {0.036, 0.024, 0.0182, 0.05};
-    
-    public static final boolean DRIVE_DEBUG = false;
-    public static final boolean STEER_DEBUG = false;
-    public static final boolean STATE_DEBUG = false;
   }
 
   public static class LoggingConstants{
     public static final String SWERVE_TABLE = "SwerveStats";
     public static final String REV_TABLE = "MotorStats";
     public static final String CTRE_TABLE = "MotorStats";
+  }
+
+  public static class DebugConstants{
+    public static final boolean STATE_DEBUG = false;
+    public static final boolean DRIVE_DEBUG = false;
+    public static final boolean STEER_DEBUG = false;
+    public static final boolean REV_DEBUG = false;
+    public static final boolean CTRE_DEBUG = false;
   }
 }
--- src/main/java/frc/robot/util/Motors/LoggedSparkMax.java
+++ src/main/java/frc/robot/util/Motors/LoggedSparkMax.java
@@ -6,10 +6,16 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEvent;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import frc.robot.util.GRTUtil;
+
+import static frc.robot.Constants.LoggingConstants.REV_TABLE;
+import static frc.robot.Constants.DebugConstants.REV_DEBUG;
+
+import java.util.EnumSet;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase.ControlType;
@@ -22,6 +28,7 @@ import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 
 public class LoggedSparkMax {
     
+    private final int canId;
     private final SparkMax motor;
     private final SparkMaxConfig sparkMaxConfig;
     private final ClosedLoopConfig closedLoopConfig;
@@ -31,18 +38,25 @@ public class LoggedSparkMax {
 
     private NetworkTableInstance ntInstance; 
     private NetworkTable motorStatsTable; 
-    private DoublePublisher neoPositionPublisher;
-    private DoublePublisher neoSetPositionPublisher; 
+    private DoublePublisher positionPublisher;
+    private DoublePublisher velocityPublisher;
+    private DoublePublisher busVoltagePublisher;
+    private DoublePublisher outputCurrentPublisher;
+    private DoublePublisher appliedOutputPublisher;
+    private DoublePublisher temperaturePublisher;
+    private DoublePublisher targetReferencePublisher;
 
     private DoubleLogEntry positionLogEntry;
-    private DoubleLogEntry targetPositionLogEntry;
+    private DoubleLogEntry velocityLogEntry;
     private DoubleLogEntry busVoltageLogEntry;
     private DoubleLogEntry outputCurrtLogEntry;
-    private DoubleLogEntry temperatureLogEntry;
     private DoubleLogEntry appliedOutputLogEntry;
+    private DoubleLogEntry temperatureLogEntry;
+    private DoubleLogEntry targetReferenceLogEntry;
 
     public LoggedSparkMax(LoggedSparkMaxConfig config) {
-        motor = new SparkMax(config.getCanId(), MotorType.kBrushless);
+        canId = config.getCanId();
+        motor = new SparkMax(canId, MotorType.kBrushless);
         sparkMaxConfig = config.getSparkMaxConfig();
         closedLoopConfig = config.getClosedLoopConfig();
         motor.configure(sparkMaxConfig,
@@ -51,8 +65,11 @@ public class LoggedSparkMax {
         closedLoopController = motor.getClosedLoopController();
         encoder = motor.getEncoder();
 
-        initLogs(config.getCanId());
-        intiNT(config.getCanId());
+        initLogs();
+        intiNT();
+        if(REV_DEBUG){
+            enableDebug();
+        }
     }
 
     /**
@@ -117,17 +134,40 @@ public class LoggedSparkMax {
     }
 
     /**
+     * Gets the velocity of the motor in RPM after taking the velocity
+     * conversion factor into account
+     */
+    public double getVelocity(){
+        return encoder.getVelocity();
+    }
+
+    /**
      * Initializes NetworkTables 
      * @param canId motor's CAN ID
      */
-    private void intiNT(int canId) {
+    private void intiNT() {
         ntInstance = NetworkTableInstance.getDefault();
-        motorStatsTable = ntInstance.getTable("MotorStats"); 
-        neoPositionPublisher = motorStatsTable.getDoubleTopic(
-            canId + "neoPosition"
+        motorStatsTable = ntInstance.getTable(REV_TABLE); 
+        positionPublisher = motorStatsTable.getDoubleTopic(
+            canId + "Position"
         ).publish(); 
-        neoSetPositionPublisher = motorStatsTable.getDoubleTopic(
-            canId + "neoSetPosition"
+        velocityPublisher = motorStatsTable.getDoubleTopic(
+            canId + "Velocity"
+        ).publish();
+        busVoltagePublisher = motorStatsTable.getDoubleTopic(
+            canId + "BusVoltage"
+        ).publish();
+        outputCurrentPublisher = motorStatsTable.getDoubleTopic(
+            canId + "OutputCurrent"
+        ).publish();
+        appliedOutputPublisher = motorStatsTable.getDoubleTopic(
+            canId + "AppliedOutput"
+        ).publish();
+        temperaturePublisher = motorStatsTable.getDoubleTopic(
+            canId + "Temperature"
+        ).publish();
+        targetReferencePublisher = motorStatsTable.getDoubleTopic(
+            canId + "TargetReference"
         ).publish();
     }
 
@@ -135,12 +175,12 @@ public class LoggedSparkMax {
      * Initializes logs
      * @param canId motor's CAN ID
      */
-    private void initLogs(int canId){
+    private void initLogs(){
         positionLogEntry =
             new DoubleLogEntry(DataLogManager.getLog(), canId + "position");
         
-        targetPositionLogEntry =
-            new DoubleLogEntry(DataLogManager.getLog(), canId + "targetPosition");
+        targetReferenceLogEntry=
+            new DoubleLogEntry(DataLogManager.getLog(), canId + "targetReference");
 
         busVoltageLogEntry =
             new DoubleLogEntry(DataLogManager.getLog(), canId + "busVoltage");
@@ -156,31 +196,54 @@ public class LoggedSparkMax {
     }
 
     /**
-     * Publishes Neo stats to NT
+     * Allows for PIDF tuning in NT
      */
-    public void publishStats(){
-        neoPositionPublisher.set(getPosition());
-        neoSetPositionPublisher.set(targetReference);
+    private void enableDebug(){
+        motorStatsTable.getDoubleArrayTopic(canId + "PIDF").publish().set(
+            new double[] {0., 0., 0., 0.}
+        );
+        motorStatsTable.addListener(canId + "PIDF", 
+            EnumSet.of(NetworkTableEvent.Kind.kValueAll),
+            (table, key, event) -> {
+                configurePIDF(
+                    event.valueData.value.getDoubleArray()[0],
+                    event.valueData.value.getDoubleArray()[1],
+                    event.valueData.value.getDoubleArray()[2],
+                    event.valueData.value.getDoubleArray()[3]
+                );
+            }
+        );
     }
 
     /**
-     * Logs Neo stats into log file
+     * Publishes  stats to NT
+     */
+    public void publishStats(){
+        positionPublisher.set(getPosition());
+        velocityPublisher.set(getVelocity());
+        busVoltagePublisher.set(motor.getBusVoltage());
+        outputCurrentPublisher.set(motor.getOutputCurrent());
+        appliedOutputPublisher.set(motor.getAppliedOutput());
+        temperaturePublisher.set(motor.getMotorTemperature());
+        targetReferencePublisher.set(targetReference);
+    }
+
+    /**
+     * Logs  stats into log file
      */
     public void logStats(){
         positionLogEntry.append(getPosition(), GRTUtil.getFPGATime());
-        targetPositionLogEntry.append(targetReference, GRTUtil.getFPGATime());
+        velocityLogEntry.append(getVelocity(), GRTUtil.getFPGATime());
         busVoltageLogEntry.append(motor.getBusVoltage(), GRTUtil.getFPGATime());
-
         outputCurrtLogEntry.append(
             motor.getOutputCurrent(), GRTUtil.getFPGATime()
         );
-
-        temperatureLogEntry.append(
-            motor.getMotorTemperature(), GRTUtil.getFPGATime()
-        );
-
         appliedOutputLogEntry.append(
             motor.getAppliedOutput(), GRTUtil.getFPGATime()
         );
+        temperatureLogEntry.append(
+            motor.getMotorTemperature(), GRTUtil.getFPGATime()
+        );
+        targetReferenceLogEntry.append(targetReference, GRTUtil.getFPGATime());
     }
 }
