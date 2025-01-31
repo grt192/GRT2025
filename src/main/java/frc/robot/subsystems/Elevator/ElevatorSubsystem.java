f129390c8aa1dea0f159ed18c46d484b2f95d149
Jesse <jesse.chu@outlook.com>
2025-01-30 16:41:38 -0800

    Using logged talon

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorSubsystem.java
@@ -1,74 +1,63 @@
 package frc.robot.subsystems.Elevator;
 
-import com.ctre.phoenix6.configs.Slot1Configs;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.DebugConstants.CTRE_DEBUG;
 import frc.robot.Constants.ElevatorConstants;
+import frc.robot.util.Motors.LoggedTalon;
 
 
 public class ElevatorSubsystem extends SubsystemBase{
 
     //motor stuff and configs
-    private final TalonFX elevatorMotor;
-    private PositionVoltage request;
-    private Slot1Configs slot1Configs;
-
-    //states
-    private ElevatorState state = ElevatorState.ZERO_STATE;
-    private ElevatorState targetState = ElevatorState.ZERO_STATE;
+    private final LoggedTalon elevatorMotor;
 
     //limit switch
     private final DigitalInput zeroLimitSwitch;
     
     public ElevatorSubsystem() {
-
         zeroLimitSwitch = new DigitalInput(ElevatorConstants.LIMIT_ID); 
-        elevatorMotor = new TalonFX(ElevatorConstants.KRAKEN_ID);
+        elevatorMotor = new LoggedTalon(
+            ElevatorConstants.KRAKEN_ID, ElevatorConstants.TALON_CONFIG
+        );
+    }
 
-        request = new PositionVoltage(0); //change
-
-        //pid stuff
-        slot1Configs = new Slot1Configs();
-        slot1Configs.kP = ElevatorConstants.ELEVATOR_P;
-        slot1Configs.kI = ElevatorConstants.ELEVATOR_I;
-        slot1Configs.kD = ElevatorConstants.ELEVATOR_D;
-        
-        elevatorMotor.getConfigurator().apply(slot1Configs);
+    @Override
+    public void periodic() {
+        if(atGround()) {
+            elevatorMotor.setPosition(0);
+        }
+        elevatorMotor.logStats();
+        if(CTRE_DEBUG || ElevatorConstants.DEBUG){
+            elevatorMotor.publishStats();
+        }
     }
 
     //functions
     public void setTargetState(ElevatorState targetState) {
-        double targetTicks = targetState.getExtendInTicks();
-        elevatorMotor.setControl(request.withPosition(targetTicks));
-        this.targetState = targetState;
+        double targetPosition = targetState.getDistance();
+        elevatorMotor.setPositionReference(targetPosition);
     }
 
-    public ElevatorState getCurrentState() {
-        return this.targetState;
+    /**
+     * Sends the elevator to the ground
+     */
+    public void toGround(){
+        elevatorMotor.setPositionReferenceWithArbFF(
+            ElevatorState.GROUND.getDistance(), ElevatorConstants.kArbFF
+        );
     }
 
     public boolean atState(ElevatorState state) {
-        double distance = Math.abs(this.getCurrentPosition() - state.getExtendInTicks());
+        double distance = Math.abs(this.getCurrentPosition() - state.getDistance());
         return distance < ElevatorConstants.TOLERANCE;
     }
 
-    //returns current position in meters (?)
     public double getCurrentPosition() {
-        double dimAnalysis = elevatorMotor.getPosition().getValueAsDouble() * ElevatorConstants.TICKS_TO_METERS;
-        return dimAnalysis;
+        return elevatorMotor.getPosition();
     }
 
     public boolean atGround() {
-        double distance = Math.abs(this.getCurrentPosition() - state.getExtendInTicks());
-        return (distance < ElevatorConstants.TOLERANCE) || zeroLimitSwitch.get();
-    }
-
-    public void periodic() {
-
-        if(atGround()) {
-            elevatorMotor.setPosition(0);
-        }
+        return zeroLimitSwitch.get();
     }
 }
