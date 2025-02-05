b6963250344f7a3eecde364dd52ed759d1127127
swaswa999 <swayams@icloud.com>
2025-02-04 18:44:14 -0800

    need to test

--- src/main/java/frc/robot/subsystems/swerve/KrakenSteerMotor.java
+++ src/main/java/frc/robot/subsystems/swerve/KrakenSteerMotor.java
@@ -1,193 +1,175 @@
 package frc.robot.subsystems.swerve;
 
+import static frc.robot.Constants.LoggingConstants.SWERVE_TABLE;
+
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage; 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.AbsoluteEncoderConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig;
-import com.revrobotics.spark.config.MAXMotionConfig;
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
-
-import static frc.robot.Constants.LoggingConstants.SWERVE_TABLE;
+import com.ctre.phoenix6.configs.ClosedLoopGeneralConfigs;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import frc.robot.util.GRTUtil;
 
-import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkBase.ControlType;
-
 public class KrakenSteerMotor {
 
     private final TalonFX motor;
-    private final CANCoder steerEncoder;
-    
-    private final TalonFXConfiguration talonFXConfig;
-    private final CANCoderConfiguration canCoderConfig;
-    private AbsoluteEncoderConfig encoderConfig;
-    private ClosedLoopConfig closedLoopConfig;
-    private MAXMotionConfig maxMotionConfig;
+    private final TalonFXConfiguration motorConfig = new TalonFXConfiguration();
+    ClosedLoopGeneralConfigs closedLoopGeneralConfigs;
 
-    private SparkClosedLoopController steerPIDController;
+    private VelocityVoltage request;
+    private double targetDouble = 0;
 
-    private NetworkTableInstance ntInstance; 
-    private NetworkTable swerveStatsTable; 
-    private DoublePublisher neoPositionPublisher;
-    private DoublePublisher neoSetPositionPublisher; 
+    private NetworkTableInstance ntInstance;
+    private NetworkTable swerveStatsTable;
+    private DoublePublisher positionPublisher;
+    private DoublePublisher targetPositionPublisher;
+    private DoublePublisher veloErrorPublisher;
+    private DoublePublisher veloPublisher;
+    private DoublePublisher appliedVoltsPublisher;
+    private DoublePublisher supplyCurrentPublisher;
+    private DoublePublisher statorCurrentPublisher;
+
+    private StatusSignal<Angle> positionSignal;
+    private StatusSignal<AngularVelocity> velocitySignal;
+    private StatusSignal<Voltage> appliedVoltsSignal;
+    private StatusSignal<Current> supplyCurrentSignal;
+    private StatusSignal<Current> statorCurrentSignal;
 
     private DoubleLogEntry positionLogEntry;
     private DoubleLogEntry targetPositionLogEntry;
     private DoubleLogEntry busVoltageLogEntry;
-    private DoubleLogEntry outputCurrtLogEntry;
+    private DoubleLogEntry outputCurrentLogEntry;
     private DoubleLogEntry temperatureLogEntry;
-    private DoubleLogEntry appliedOutputLogEntry; //pplied output duty cycle.
+    private DoubleLogEntry appliedOutputLogEntry;
 
-    private double targetDouble = 0;
-
-    /**
-     * A Neo steer motor for swerve steering
-     * @param canId the motor's CAN ID
-     */
     public KrakenSteerMotor(int canId) {
+        motor = new TalonFX(canId, "can");
 
-        motor = new SparkMax(canId, MotorType.kBrushless);
+        // Configure motor settings
+        motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-        encoderConfig = new AbsoluteEncoderConfig();
-        encoderConfig.inverted(true);
+        //max motor voltage is 12V
+        motorConfig.Voltage.PeakForwardVoltage = 12; 
+        motorConfig.Voltage.PeakReverseVoltage = -12;
 
-        maxMotionConfig = new MAXMotionConfig();
-        maxMotionConfig.allowedClosedLoopError(.005);
-
-        closedLoopConfig = new ClosedLoopConfig();
-        closedLoopConfig.feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-                        .positionWrappingEnabled(true)
-                        .positionWrappingMinInput(0)
-                        .positionWrappingMaxInput(1)
-                        .apply(maxMotionConfig);
+        // Enable position wrapping (by default values are from 0-1)
+        closedLoopGeneralConfigs = new ClosedLoopGeneralConfigs();
+        closedLoopGeneralConfigs.ContinuousWrap = true; //basicaly turns stacking off
+        motorConfig.ClosedLoopGeneral = closedLoopGeneralConfigs;
         
-        sparkMaxConfig = new SparkMaxConfig();
-        sparkMaxConfig.apply(closedLoopConfig)
-                      .apply(encoderConfig);
+        // Set initial position to 0
+        motor.setPosition(0);
+        for (int i = 0; i < 4; i++) {
+            boolean error = motor.getConfigurator().apply(motorConfig, 0.1) == StatusCode.OK;
+            if (!error) break;
+        }
 
-        motor.configure(sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        
-        steerEncoder = motor.getAbsoluteEncoder();
-        // motor.setCANTimeout(250);
-
-        // motor.setPeriodicFramePeriod(PeriodicFrame.kStatus5, 20); //increase position update frequency
-        // motor.setPeriodicFramePeriod(PeriodicFrame.kStatus6, 20); //increase velocity update frequency
-
-        // motor.setSmartCurrentLimit(40);
-        // motor.enableVoltageCompensation(12.0);
-
-        // motor.burnFlash();
         initNT(canId);
+        initSignals();
         initLogs(canId);
     }
 
     /**
-     * Configures the motor's PID
-     * @param p kP
-     * @param i kI
-     * @param d kD
-     * @param ff kFF
+     * Initializes NetworkTables for publishing motor statistics.
+     * @param canId The CAN ID of the motor.
      */
-    
-    public void configurePID(double p, double i, double d, double ff){
-        closedLoopConfig.pidf(p, i, d, ff);
-        sparkMaxConfig.apply(closedLoopConfig);
-        motor.configure(sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        steerPIDController = motor.getClosedLoopController();
+    private void initNT(int canId) {
+        ntInstance = NetworkTableInstance.getDefault();
+        swerveStatsTable = ntInstance.getTable(SWERVE_TABLE);
+        positionPublisher = swerveStatsTable.getDoubleTopic(canId + "position").publish();
+        targetPositionPublisher = swerveStatsTable.getDoubleTopic(canId + "targetPosition").publish();
+        veloErrorPublisher = swerveStatsTable.getDoubleTopic(canId + "veloError").publish();
+        veloPublisher = swerveStatsTable.getDoubleTopic(canId + "velo").publish();
+        appliedVoltsPublisher = swerveStatsTable.getDoubleTopic(canId + "appliedVolts").publish();
+        supplyCurrentPublisher = swerveStatsTable.getDoubleTopic(canId + "supplyCurrent").publish();
+        statorCurrentPublisher = swerveStatsTable.getDoubleTopic(canId + "statorCurrent").publish();
     }
 
     /**
-     * Using PID to move to target position
-     * @param targetRads target position in radiants
+     * Initializes the status signals for the motor.
+     */
+    private void initSignals() {
+        positionSignal = motor.getPosition();
+        velocitySignal = motor.getVelocity();
+        appliedVoltsSignal = motor.getMotorVoltage();
+        statorCurrentSignal = motor.getStatorCurrent();
+        supplyCurrentSignal = motor.getSupplyCurrent();
+
+        // Set update frequency for all signals
+        BaseStatusSignal.setUpdateFrequencyForAll(
+            250.0, positionSignal, velocitySignal,
+            appliedVoltsSignal, statorCurrentSignal, supplyCurrentSignal
+        );
+        motor.optimizeBusUtilization(0, 1.0);
+    }
+
+    /**
+     * Initializes the data logs for the motor.
+     * @param canId The CAN ID of the motor.
+     */
+    private void initLogs(int canId) {
+        positionLogEntry = new DoubleLogEntry(DataLogManager.getLog(), canId + "position");
+        targetPositionLogEntry = new DoubleLogEntry(DataLogManager.getLog(), canId + "targetPosition");
+        busVoltageLogEntry = new DoubleLogEntry(DataLogManager.getLog(), canId + "busVoltage");
+        outputCurrentLogEntry = new DoubleLogEntry(DataLogManager.getLog(), canId + "outputCurrent");
+        temperatureLogEntry = new DoubleLogEntry(DataLogManager.getLog(), canId + "temperature");
+        appliedOutputLogEntry = new DoubleLogEntry(DataLogManager.getLog(), canId + "appliedOutput");
+    }
+
+    /**
+     * Configures the PID constants for the motor.
+     * @param p Proportional gain.
+     * @param i Integral gain.
+     * @param d Derivative gain.
+     * @param s Static gain.
+     * @param v Velocity gain.
+     */
+    public void configurePID(double p, double i, double d, double s, double v) {
+        Slot0Configs slot0Configs = new Slot0Configs();
+        slot0Configs.kP = p;
+        slot0Configs.kI = i;
+        slot0Configs.kD = d;
+        slot0Configs.kS = s;
+        slot0Configs.kV = v;
+        motor.getConfigurator().apply(slot0Configs);
+    }
+
+    /**
+     * Sets the target position for the motor.
+     * @param targetRads The target position in radians.
      */
     public void setPosition(double targetRads) {
-        targetDouble = (targetRads + Math.PI) / (2. * Math.PI);
-        steerPIDController.setReference(targetDouble, ControlType.kPosition);
+        targetDouble = targetRads / (2 * Math.PI); //turn into a value between 0 and 1
+        motor.setControl(request);
     }
 
     /**
-     * Gets the motor's position through the absolute encoder
-     * @return position in double from 0 to 1
+     * Gets the current position of the motor.
+     * @return The current position in radians.
      */
-    public double getPosition(){
-        return steerEncoder.getPosition();
+    public double getPosition() {
+        return motor.getPosition().getValueAsDouble() * 2 * Math.PI;
     }
 
     /**
-     * Initializes NetworkTables
-     * @param canId
+     * Publishes the motor statistics to NetworkTables.
      */
-    private void initNT (int canId){
-        ntInstance = NetworkTableInstance.getDefault();
-        swerveStatsTable = ntInstance.getTable(SWERVE_TABLE); 
-        neoPositionPublisher = swerveStatsTable.getDoubleTopic(canId + "neoPosition").publish(); 
-        neoSetPositionPublisher = swerveStatsTable.getDoubleTopic(canId + "neoSetPosition").publish();
-    }
-    
-    private void initLogs(int canId){
-        positionLogEntry =
-            new DoubleLogEntry(DataLogManager.getLog(), canId + "position");
-        
-        targetPositionLogEntry =
-            new DoubleLogEntry(DataLogManager.getLog(), canId + "targetPosition");
-
-        busVoltageLogEntry =
-            new DoubleLogEntry(DataLogManager.getLog(), canId + "busVoltage");
-        
-        outputCurrtLogEntry =
-            new DoubleLogEntry(DataLogManager.getLog(), canId + "outputCurrent");
-        
-        temperatureLogEntry =
-            new DoubleLogEntry(DataLogManager.getLog(), canId + "temperature");
-        
-        appliedOutputLogEntry =
-            new DoubleLogEntry(DataLogManager.getLog(), canId + "appliedOutput");
-    }
-
-    /**
-     * Publishes Neo stats to NT
-     */
-    public void publishStats(){
-
-        neoPositionPublisher.set(getPosition());
-        neoSetPositionPublisher.set(targetDouble);
-    }
-
-    public void logStats(){
-        positionLogEntry.append(getPosition(), GRTUtil.getFPGATime());
-        targetPositionLogEntry.append(targetDouble, GRTUtil.getFPGATime());
-        busVoltageLogEntry.append(motor.getBusVoltage(), GRTUtil.getFPGATime());
-
-        outputCurrtLogEntry.append(
-            motor.getOutputCurrent(), GRTUtil.getFPGATime()
-        );
-
-        temperatureLogEntry.append(
-            motor.getMotorTemperature(), GRTUtil.getFPGATime()
-        );
-
-        appliedOutputLogEntry.append(
-            motor.getAppliedOutput(), GRTUtil.getFPGATime()
-        );
+    public void publishStats() {
+        positionPublisher.set(getPosition());
+        targetPositionPublisher.set(targetDouble);
     }
 }
