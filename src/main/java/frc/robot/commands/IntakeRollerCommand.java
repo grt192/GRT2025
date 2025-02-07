5bc5885c3fe5277589b5358cd419f5e7406a4a78
swaswa999 <swayams@icloud.com>
2025-02-07 14:42:00 -0800

    intake roller done

--- src/main/java/frc/robot/commands/IntakeRollerCommand.java
+++ src/main/java/frc/robot/commands/IntakeRollerCommand.java
@@ -8,10 +8,17 @@ public class IntakeRollerCommand {
     private double speed;
 
     public IntakeRollerCommand(IntakeRollerSubsystem intakeRollerSubsystem) {
+        this.intakeRollerSubsystem = intakeRollerSubsystem;
+        this.speed = 0.5;
 
     }
 
     public void execute() {
+        if (intakeRollerSubsystem.getSpeed() == 0) {
+            intakeRollerSubsystem.setSpeed(speed);
+        } else {
+            intakeRollerSubsystem.stop();
+        }
 
     }
 
