260f9364f6c2041576376b3c077a35a81669a657
swaswa999 <swayams@icloud.com>
2025-02-07 14:47:56 -0800

    rollers command done

--- src/main/java/frc/robot/commands/IntakeRollerCommand.java
+++ src/main/java/frc/robot/commands/IntakeRollerCommand.java
@@ -1,21 +1,24 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Intake.IntakeRollerSubsystem;
+import frc.robot.Constants;
 
 public class IntakeRollerCommand {
 
     private IntakeRollerSubsystem intakeRollerSubsystem;
-    private double speed;
+    private double rollerSpeed;
+
 
     public IntakeRollerCommand(IntakeRollerSubsystem intakeRollerSubsystem) {
         this.intakeRollerSubsystem = intakeRollerSubsystem;
-        this.speed = 0.5;
+        this.rollerSpeed = Constants.DifferentialRollerConstants.ROLLER_SPEED;
+    
 
     }
 
     public void execute() {
         if (intakeRollerSubsystem.getSpeed() == 0) {
-            intakeRollerSubsystem.setSpeed(speed);
+            intakeRollerSubsystem.setSpeed(rollerSpeed);
         } else {
             intakeRollerSubsystem.stop();
         }
