3f549200975f2ffe5b6fe5fca76d13ffcc834b37
swaswa999 <swayams@icloud.com>
2025-02-14 14:41:29 -0800

    thingy

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,7 +41,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     constructDriveController(); 
-    startLog();
+    // startLog();
     setVisionDataInterface();
     configureBindings();
   }
