c360675402c0e0e80075f5141ff664684b6e9804
siyoyoCode <siyona101215@gmail.com>
2025-02-19 12:03:54 -0800

    formatting

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorState.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorState.java
@@ -1,32 +1,32 @@
-package frc.robot.subsystems.Elevator;
+package frc.robot.subsystems.elevator;
 
 import frc.robot.Constants.ElevatorConstants;
 
 /** States of the elevator. */
 public enum ElevatorState {
-    GROUND(ElevatorConstants.GROUND),
-    SOURCE(ElevatorConstants.SOURCE),
-    L1(ElevatorConstants.L1), //currently at test vlue
-    L2(ElevatorConstants.L2),
-    L3(ElevatorConstants.L3),
-    L4(ElevatorConstants.L4);
-
-    private final double distance;
-
-    private ElevatorState(double distance) {
-        this.distance = distance;
-    }
-
-    /**
-     * Gets the distance of a state in meters.
-     *
-     * @return meters in double.
-     */
-    public double getDistance() {
-        return this.distance;
-    }
-
-    public double getDistanceInTicks() { 
-        return this.distance * ElevatorConstants.dutyCycletoticks;
-    }
+  GROUND(ElevatorConstants.GROUND),
+  SOURCE(ElevatorConstants.SOURCE),
+  L1(ElevatorConstants.L1), //currently at test vlue
+  L2(ElevatorConstants.L2),
+  L3(ElevatorConstants.L3),
+  L4(ElevatorConstants.L4);
+  
+  private final double distance;
+  
+  private ElevatorState(double distance) {
+    this.distance = distance;
+  }
+  
+  /**
+  * Gets the distance of a state in meters.
+  *
+  * @return meters in double.
+  */
+  public double getDistance() {
+    return this.distance;
+  }
+  
+  public double getDistanceInTicks() { 
+    return this.distance * ElevatorConstants.dutyCycletoticks;
+  }
 }
