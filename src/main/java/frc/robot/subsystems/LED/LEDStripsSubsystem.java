b85829b296b8e13fc65154b36ed917a3bd8d6dd5
swaswa999 <swayams@icloud.com>
2025-02-22 17:35:14 -0800

    update to change to all colors, also to pulse and rainbow :D

--- src/main/java/frc/robot/subsystems/LED/LEDStripsSubsystem.java
+++ src/main/java/frc/robot/subsystems/LED/LEDStripsSubsystem.java
@@ -1,15 +1,15 @@
+package frc.robot.subsystems.LED;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
-package frc.robot.subsystems.LED;
-
-public class LEDStrips {
+public class LEDStripsSubsystem {
 
 private AddressableLED led;
 private AddressableLEDBuffer ledBuffer;
 private int length;
 
-public LEDStrips(int pwmPort, int length) {
+public LEDStripsSubsystem(int pwmPort, int length) {
     this.length = length;
     led = new AddressableLED(pwmPort);
     ledBuffer = new AddressableLEDBuffer(length);
@@ -33,6 +33,45 @@ public void setAllLEDsColor(int r, int g, int b) {
     led.setData(ledBuffer);
 }
 
+public void setRed() {
+    setAllLEDsColor(255, 0, 0);
+}
+
+public void setGreen() {
+    setAllLEDsColor(0, 255, 0);
+}
+
+public void setBlue() {
+    setAllLEDsColor(0, 0, 255);
+}
+
+public void setRainbow() {
+    for (int i = 0; i < length; i++) {
+        final int hue = (i * 180 / length) % 180;
+        ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    led.setData(ledBuffer);
+}
+
+public void pulseLEDs(int r, int g, int b, int delay) {
+    new Thread(() -> {
+        try {
+            while (true) {
+                for (int i = 0; i <= 255; i++) {
+                    setAllLEDsColor((r * i) / 255, (g * i) / 255, (b * i) / 255);
+                    Thread.sleep(delay);
+                }
+                for (int i = 255; i >= 0; i--) {
+                    setAllLEDsColor((r * i) / 255, (g * i) / 255, (b * i) / 255);
+                    Thread.sleep(delay);
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }).start();
+}
+
 public void turnOffLEDs() {
     for (int i = 0; i < length; i++) {
         ledBuffer.setRGB(i, 0, 0, 0);
@@ -43,5 +82,6 @@ public void turnOffLEDs() {
 public int getLength() {
     return length;
 }
-k
+
 }
+
