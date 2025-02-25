d68fdc77f779b1281b1abef281b0463ade0b9e7d
Jesse <jesse.chu@outlook.com>
2025-02-25 15:23:23 -0800

    new drive PID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,9 +63,9 @@ public final class Constants {
     public static final double RAMP_RATE = 0;
     // public static final double KT = 0.01937; // Torque constant in Nm/A, refer to ctre Motor Performance Analysis Report
 
-    public static final double[] DRIVE_P = new double[] {10, 10, 10, 10}; //.32
+    public static final double[] DRIVE_P = new double[] {9.5, 9.5, 9,5, 9.5}; //.32
     public static final double[] DRIVE_I = new double[] {0, 0, 0, 0}; 
-    public static final double[] DRIVE_D = new double[] {0.01, 0.01, 0.01, 0.01};
+    public static final double[] DRIVE_D = new double[] {0.1, 0.1, 0.1, 0.1};
     public static final double[] DRIVE_S = new double[] {5, 5, 5, 5};//{0.16, 0.1499, 0.1499, 0.1499};
     public static final double[] DRIVE_V = new double[] {0.0, 0.0, 0.0, 0.0}; //{0.11, 0.112, 0.112, 0.112};
 
@@ -75,7 +75,7 @@ public final class Constants {
     public static final double[] STEER_D = new double[] {0, 0, 0, 0};
     public static final double[] STEER_FF = new double[] {0.023,.02,0.025,0.03}; //{0.036, 0.024, 0.0182, 0.05};
     
-    public static final boolean DRIVE_DEBUG = true;
+    public static final boolean DRIVE_DEBUG = false;
     public static final boolean STEER_DEBUG = false;
     public static final boolean STATE_DEBUG = false;
   }
