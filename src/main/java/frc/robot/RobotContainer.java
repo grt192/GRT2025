09ca417a8bb68e19d9901d43554cbde64dd54c8c
Tony Wu <tonyxywu@gmail.com>
2025-02-26 11:24:39 -0800

    cam 1 & 2 construction and nt table camera selectr

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,17 @@ import frc.robot.controllers.DualJoystickDriveController;
 import frc.robot.controllers.PS5DriveController;
 import frc.robot.controllers.XboxDriveController;
 
+import java.util.EnumSet;
+
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableEvent;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.util.PixelFormat;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -35,12 +44,25 @@ public class RobotContainer {
 
   private final FieldManagementSubsystem fieldManagementSubsystem = new FieldManagementSubsystem();
   private final PhoenixLoggingSubsystem phoenixLoggingSubsystem = new PhoenixLoggingSubsystem(fieldManagementSubsystem);
+  
+  //driver camera stuff:
+  private UsbCamera driverCamera;
+  private MjpegServer driverCameraServer;
+  private UsbCamera driverCamera2;
+
+  private NetworkTableInstance ntInstance;
+  private NetworkTable testTable; 
+  private NetworkTable FMStable;
+  private NetworkTableEntry cameraSelectionEntry;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     constructDriveController(); 
     startLog();
     configureBindings();
+    constructDriverCameras();
+    constructNetworkTableListeners();
+
   }
 
   /**
@@ -113,4 +135,55 @@ public class RobotContainer {
     DriverStation.startDataLog(DataLogManager.getLog());
 
   }
+
+  public void constructDriverCameras(){
+    try {
+      driverCamera = new UsbCamera("fisheye", 0);
+      driverCamera.setVideoMode(PixelFormat.kMJPEG, 160, 120, 30);
+      driverCamera.setExposureManual(40);
+      driverCameraServer = new MjpegServer("m1", 1181);
+      driverCameraServer.setSource(driverCamera);
+    } catch (Exception e) {
+      System.out.print(e);
+    }
+    try {
+      driverCamera2 = new UsbCamera("fisheye2", 1);
+      driverCamera2.setVideoMode(PixelFormat.kMJPEG, 160, 120, 30);
+      driverCamera2.setExposureManual(40);
+    } catch (Exception e) {
+      System.out.print(e);
+    }
+  }
+
+  public void constructNetworkTableListeners(){
+
+    ntInstance = NetworkTableInstance.getDefault();
+    FMStable = ntInstance.getTable("FMSInfo");
+    testTable = ntInstance.getTable("testTable");
+
+    //allianceEntry = FMStable.getEntry("IsRedAlliance");
+    cameraSelectionEntry = testTable.getEntry("cameraSelection");
+    // FMStable.addListener("IsRedAlliance", EnumSet.of(NetworkTableEvent.Kind.kValueAll), (table, key, event) -> {
+            
+    //     });
+    testTable.addListener("cameraSelection", EnumSet.of(NetworkTableEvent.Kind.kValueAll), (table, key, event) ->{
+      if (event.valueData.value.getBoolean()){
+        //server1.setSource(camera1);
+        driverCameraServer.setSource(driverCamera);
+
+        System.out.println("CAMERA1!");
+      }
+      else{
+        //server1.setSource(camera2);
+        driverCameraServer.setSource(driverCamera2);
+
+        System.out.println("CAMERA2!");
+
+      }
+    });
+
+  }
+
+
+
 }
