0a6048869131991c2d848e71dc42d171ad1db6eb
CrolineCrois <maoninii@gmail.com>
2025-03-29 12:59:50 -0600

    quals day 2

--- src/main/java/frc/robot/subsystems/Climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/Climb/ClimbSubsystem.java
@@ -45,7 +45,7 @@ public class ClimbSubsystem extends SubsystemBase {
       .withSoftwareLimitSwitch(
         new SoftwareLimitSwitchConfigs()
           .withForwardSoftLimitEnable(true)
-          .withForwardSoftLimitThreshold(60)
+          .withForwardSoftLimitThreshold(57)
       );
       
 
@@ -58,7 +58,7 @@ public class ClimbSubsystem extends SubsystemBase {
     if(DebugConstants.MASTER_DEBUG || ClimbConstants.CLIMB_DEBUG){
       motor.publishStats();
     }
-    // System.out.println(motor.getPosition());
+    System.out.println(motor.getPosition());
   }
 
   /**
